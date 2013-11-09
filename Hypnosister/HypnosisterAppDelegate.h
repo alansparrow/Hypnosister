7ac88847b27e8a270dbc9546173b2b85185bfb78
Alan Sparrow <trannguyenbaotrung@gmail.com>
2013-11-09 09:30:39 +0900

    Initial Commit

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Alan Sparrow on 11/9/13.
+//  Copyright (c) 2013 Alan Sparrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
