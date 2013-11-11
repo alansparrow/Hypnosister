6ed2fe7b37c9dbc4f8ddeb7e613355c75342c7da
Alan Sparrow <trannguyenbaotrung@gmail.com>
2013-11-11 09:13:35 +0900

    Finish Panning, paging, zooming and hiding the Status Bar

--- Hypnosister/HypnosisterAppDelegate.m
+++ Hypnosister/HypnosisterAppDelegate.m
@@ -14,12 +14,22 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
     // Override point for customization after application launch.
     
     CGRect screenRect = [[self window] bounds];
     
     // Create the UIScrollView to have the size of the window, matching its size
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:screenRect];
+//    [scrollView setPagingEnabled:YES];
+    
+    [scrollView setMinimumZoomScale:1.0];
+    [scrollView setMaximumZoomScale:5.0];
+    
+    // You will get a warning here, ignore it for now
+    [scrollView setDelegate:self];
+    
     [[self window] addSubview:scrollView];
     
 //    CGRect viewFrame = CGRectMake(160, 240, 100, 150);
@@ -30,14 +40,21 @@
     
     // Create the HypnosisView with a frame that is twice the size of the screen
     CGRect bigRect = screenRect;
-    bigRect.size.width *= 2.0;
-    bigRect.size.height *= 2.0;
-    HypnosisView *view = [[HypnosisView alloc] initWithFrame:bigRect];
+//    bigRect.size.width *= 2.0;
+//    bigRect.size.height *= 2.0;
+//    HypnosisView *view = [[HypnosisView alloc] initWithFrame:bigRect];
+//    HypnosisView *view = [[HypnosisView alloc] initWithFrame:screenRect];
+    view = [[HypnosisView alloc] initWithFrame:screenRect];
     
 //    [[self window] addSubview:view];
     
     // Add the HypnosisView as a subview of the scrollView instead of the window
     [scrollView addSubview:view];
+    
+    // Move the rectangle for the other HypnosisView to the right, just off the screen
+//    screenRect.origin.x = screenRect.size.width;
+//    HypnosisView *anotherView = [[HypnosisView alloc] initWithFrame:screenRect];
+//    [scrollView addSubview:anotherView];
     
     // Tell the scrollView how big its virtual world is
     [scrollView setContentSize:bigRect.size];
@@ -87,6 +104,11 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return view;
 }
 
 @end
